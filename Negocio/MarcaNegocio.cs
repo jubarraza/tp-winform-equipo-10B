6ff6754a81d96234013fc12ea265cd17dfa4b96d
Julieta <jubarraza5@gmail.com>
2024-09-07 21:10:55 -0300

    creacion de MarcaNegocio
    Se crea con una funcion de Listar

---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> Listar()
+        {
+
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT Id, Descripcion as Nombre FROM MARCAS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+
+        }
+
+    }
+
+}
