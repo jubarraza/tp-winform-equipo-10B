63d22a9173081a5c7d1e871044cccd5a2d402434
jonatanguzman <jonatanguzman2712@gmail.com>
2024-09-08 10:27:10 -0300

    Public Clase Categoria

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+
+            return lista;
+        }
+
+
+
+    }
+}
