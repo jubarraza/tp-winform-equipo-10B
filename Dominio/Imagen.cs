19a890174731aa2a06db7b508cc5285dc3a21715
Damian Di Giovanni <demian-dg@hotmail.com>
2024-09-08 21:09:53 -0300

    Se agrega la clase imagen e ImagenDominio

---
+++ Dominio/Imagen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Imagen
+    {
+        public int Id { get; set; }
+
+        public int IdArticulo { get; set; }
+
+        public string ImagenURl { get; set; }
+
+        public override string ToString()
+        {
+            return ImagenURl;
+        }
+
+    }
+}
